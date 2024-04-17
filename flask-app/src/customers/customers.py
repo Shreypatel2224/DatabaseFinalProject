61081fd01b3a0d815b9027fb93c47e3b29e09d4a
BenoitSchiermeier <132936530+BenoitSchiermeier@users.noreply.github.com>
2024-04-16 21:34:18 -0400

    added user route

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -8,14 +8,46 @@ from src import db
 
 
 customers = Blueprint('customers', __name__)
-User = Blueprint('User', __name__)
+user = Blueprint('User', __name__)
 
-# Get all customers from the DB
-@customers.route('/customers', methods=['GET'])
-def get_customers():
+# # Get all customers from the DB
+# @customers.route('/customers', methods=['GET'])
+# def get_customers():
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select company, last_name,\
+#         first_name, job_title, business_phone from customers')
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         json_data.append(dict(zip(row_headers, row)))
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
+
+# # Get customer detail for customer with particular userID
+# @customers.route('/customers/<userID>', methods=['GET'])
+# def get_customer(userID):
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select * from customers where id = {0}'.format(userID))
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         json_data.append(dict(zip(row_headers, row)))
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
+
+
+
+# Get all users from the DB
+@user.route('/user', methods=['GET'])
+def get_users():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
+    cursor.execute('select User_ID, Username, User_Type, Password, Email')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -25,47 +57,5 @@ def get_customers():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
-
-# Get customer detail for customer with particular userID
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-
-
-# from flask import Blueprint, request, jsonify
-
-# user_blueprint = Blueprint('User', __name__)
-
-# @user_blueprint.route('/User', methods=['GET'])
-# def get_users():
-#     users = get_all_users()  # You need to define this function
-#     return jsonify(users), 200
-
-# @user_blueprint.route('/User', methods=['POST'])
-# def add_user():
-#     data = request.json
-#     if not data:
-#         return jsonify({'error': 'No input data provided'}), 400
-#     new_user = create_user(data)  # You need to define this function
-#     return jsonify(new_user), 201
-
-# @user_blueprint.route('/User/<int:user_id>', methods=['DELETE'])
-# def delete_user(user_id):
-#     result = delete_user_by_id(user_id)  # You need to define this function
-#     if result:
-#         return jsonify({'success': 'User deleted'}), 200
-#     else:
-#         return jsonify({'error': 'User not found'}), 404
 
 
