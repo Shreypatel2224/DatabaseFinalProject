b21d8d2d22a10628a7946b12aa533f48a1344355
BenoitSchiermeier <132936530+BenoitSchiermeier@users.noreply.github.com>
2024-04-17 16:51:04 -0400

    cp update

--- flask-app/src/CompensationPackage.py
+++ flask-app/src/CompensationPackage.py
@@ -6,6 +6,24 @@ from src import db
 compensationpackage = Blueprint('CompensationPackage', __name__)
 
 
+# Get all users from the DB
+@compensationpackage.route('/CompensationPackage', methods=['GET'])
+def get_all_compensation_packages():
+    cursor = db.get_db().cursor()
+    # cursor.execute('select User_ID, Username, User_Type, Password, Email FROM User')
+    cursor.execute('select * FROM CompensationPackage')
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
 # Get customer detail for customer with particular userID
 @compensationpackage.route('/CompensationPackage/<Position_ID>', methods=['GET'])
 def get_CP(Position_ID):
