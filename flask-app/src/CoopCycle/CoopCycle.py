c57a4278a709e6794d965890a2ee58b9d267b438
BenoitSchiermeier <132936530+BenoitSchiermeier@users.noreply.github.com>
2024-04-17 01:20:48 -0400

    Added post coopcycle

--- flask-app/src/CoopCycle/CoopCycle.py
+++ flask-app/src/CoopCycle/CoopCycle.py
@@ -19,3 +19,32 @@ def get_coopcycle():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+@coopcycle.route('/CoopCycle', methods=['POST'])
+def add_CoopCycle():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    Year = data.get('Year')
+    PlacementRate = data.get('PlacementRate')
+    Type = data.get('Type')
+
+    if not all([Year, PlacementRate, Type]):
+        return jsonify({'error': 'Missing required data'}), 400
+
+    try:
+        cursor = db.get_db().cursor()
+        cursor.execute(
+            'INSERT INTO CoopCycle (Year, PlacementRate, Type) VALUES (%s, %s, %s)',
+            (Year, PlacementRate, Type)
+        )
+        db.get_db().commit()  # commit
+        cc_id = cursor.lastrowid # ge the auto incremented id 
+        return jsonify({'success': 'CoopCycle added', 'CoopCycle_ID': cc_id}), 201  
+    except Exception as e:
+        db.get_db().rollback()  # Roll back if error
+        return jsonify({'error': 'Failed to add CoopCycle: {}'.format(str(e))}), 500
+    finally:
+        cursor.close() 
