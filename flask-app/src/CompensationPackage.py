8bfb3541543ae29f114ee7aeeda29e37b1376460
aahiljub1 <aahiljubair@gmail.com>
2024-04-17 03:31:42 -0400

    updated routes

--- flask-app/src/CompensationPackage.py
+++ flask-app/src/CompensationPackage.py
@@ -58,3 +58,95 @@ def create_CP():
         return make_response(jsonify({'error': 'Failed to create Compensation Package'}), 500)
     finally:
         cursor.close()
+
+# Delete compensation package by CPID
+@compensationpackage.route('/CompensationPackage/<CPID>', methods=['DELETE'])
+def delete_CP(CPID):
+    try:
+        cursor = db.get_db().cursor()
+        cursor.execute('DELETE FROM CompensationPackage WHERE CPID = %s', (CPID,))
+        db.get_db().commit()
+        if cursor.rowcount == 0:
+            return make_response(jsonify({'error': 'CompensationPackage not found'}), 404)
+        return make_response(jsonify({'success': 'CompensationPackage deleted'}), 200)
+    except Exception as e:
+        db.get_db().rollback()
+        return make_response(jsonify({'error': 'Failed to delete CompensationPackage'}), 500)
+    finally:
+        cursor.close()
+
+# Update compensation package by CPID
+@compensationpackage.route('/CompensationPackage/<CPID>', methods=['PUT'])
+def update_CP(CPID):
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT * FROM CompensationPackage WHERE CPID = %s", (CPID,))
+    if cursor.rowcount == 0:
+        return jsonify({'error': 'CompensationPackage not found'}), 404
+    updates = []
+    values = []
+    for field in ['Perks', 'Hourly', 'BonusTotal', 'HousingTotal', 'RelocationTotal', 'Position_ID']:
+        if field in data:
+            updates.append(f"{field} = %s")
+            values.append(data[field])
+    
+    if not updates:
+        return jsonify({'error': 'There were no valid fields to update'}), 400
+    
+    update_stmt = "UPDATE CompensationPackage SET " + ", ".join(updates) + " WHERE CPID = %s"
+    values.append(CPID)
+    
+    try:
+        cursor.execute(update_stmt, values)
+        if cursor.rowcount == 0:
+            db.get_db().rollback()
+            return jsonify({'error': 'No CompensationPackage updated'}), 400
+        else:
+            db.get_db().commit()
+            return jsonify({'message': 'CompensationPackage updated successfully'}), 200
+    except Exception as e:
+        db.get_db().rollback()
+        return jsonify({'error': str(e)}), 500
+
+# Get compensation package by bonus total
+@compensationpackage.route('/CompensationPackage/<BonusTotal>', methods=['GET'])
+def get_CP_by_BT(BonusTotal):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from CompensationPackage where BonusTotal = {0}'.format(BonusTotal))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        row = (dict(zip(row_headers, row)))
+        for key, value in row.items():
+            if isinstance(value, bytes): 
+                row[key] = value.decode('utf-8')
+        json_data.append(row)
+
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get compensation package by hourly rate
+@compensationpackage.route('/CompensationPackage/<Hourly>', methods=['GET'])
+def get_CP_by_hourly(Hourly):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from CompensationPackage where Hourly = {0}'.format(Hourly))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        row = (dict(zip(row_headers, row)))
+        for key, value in row.items():
+            if isinstance(value, bytes): 
+                row[key] = value.decode('utf-8')
+        json_data.append(row)
+
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
--- flask-app/src/Question.py
+++ flask-app/src/Question.py
@@ -50,3 +50,94 @@ def create_question():
     finally:
         cursor.close()
 
+# Delete route to delete an question based on its question_id
+@question.route('/Question/<Question_ID>', methods=['DELETE'])
+def delete_question(Question_ID):
+    try:
+        cursor = db.get_db().cursor()
+        cursor.execute('DELETE FROM Question WHERE Question_ID = %s', (Question_ID,))
+        db.get_db().commit()
+        if cursor.rowcount == 0:
+            return make_response(jsonify({'error': 'Question not found'}), 404)
+        return make_response(jsonify({'success': 'Question deleted'}), 200)
+    except Exception as e:
+        db.get_db().rollback()
+        return make_response(jsonify({'error': 'Failed to delete Question'}), 500)
+    finally:
+        cursor.close()
+
+# Update question by question id
+@question.route('/Question/<Question_ID>', methods=['PUT'])
+def update_question(Question_ID):
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT * FROM Question WHERE Question_ID = %s", (Question_ID,))
+    if cursor.rowcount == 0:
+        return jsonify({'error': 'Question not found'}), 404
+    updates = []
+    values = []
+    for field in ['Title', 'Date', 'Status', 'Content', 'User_ID']:
+        if field in data:
+            updates.append(f"{field} = %s")
+            values.append(data[field])
+    
+    if not updates:
+        return jsonify({'error': 'There were no valid fields to update'}), 400
+    
+    update_stmt = "UPDATE Question SET " + ", ".join(updates) + " WHERE Question_ID = %s"
+    values.append(Question_ID)
+
+    try:
+        cursor.execute(update_stmt, values)
+        if cursor.rowcount == 0:
+            db.get_db().rollback()
+            return jsonify({'error': 'No Question updated'}), 400
+        else:
+            db.get_db().commit()
+            return jsonify({'message': 'Question updated successfully'}), 200
+    except Exception as e:
+        db.get_db().rollback()
+        return jsonify({'error': str(e)}), 500
+
+# Get question by question id
+@question.route('/Question/<Question_ID>', methods=['GET'])
+def get_question_by_ID(Question_ID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Question where Question_ID = {0}'.format(Question_ID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        row = (dict(zip(row_headers, row)))
+        for key, value in row.items():
+            if isinstance(value, bytes): 
+                row[key] = value.decode('utf-8')
+        json_data.append(row)
+
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get question by user id
+@question.route('/Question/<Question_ID>', methods=['GET'])
+def get_question_by_UID(Question_ID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Question where Question_ID = {0}'.format(Question_ID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        row = (dict(zip(row_headers, row)))
+        for key, value in row.items():
+            if isinstance(value, bytes): 
+                row[key] = value.decode('utf-8')
+        json_data.append(row)
+
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
