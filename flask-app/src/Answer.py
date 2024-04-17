e266d8ffafa097d5e1889fb25c07b4be33380d45
BenoitSchiermeier <132936530+BenoitSchiermeier@users.noreply.github.com>
2024-04-17 19:13:49 -0400

    answer fix

--- flask-app/src/Answer.py
+++ flask-app/src/Answer.py
@@ -139,23 +139,23 @@ def get_answer_by_Question(Question_ID):
     return the_response
 
 
-# Get answer by user id
-@answer.route('/Answer/<User_ID>', methods=['GET'])
-def get_answer_by_UID(User_ID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from Answer where Answer_ID = {0}'.format(User_ID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        row = (dict(zip(row_headers, row)))
-        for key, value in row.items():
-            if isinstance(value, bytes): 
-                row[key] = value.decode('utf-8')
-        json_data.append(row)
+# # Get answer by user id
+# @answer.route('/Answer/<User_ID>', methods=['GET'])
+# def get_answer_by_UID(User_ID):
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select * from Answer where Answer_ID = {0}'.format(User_ID))
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         row = (dict(zip(row_headers, row)))
+#         for key, value in row.items():
+#             if isinstance(value, bytes): 
+#                 row[key] = value.decode('utf-8')
+#         json_data.append(row)
 
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
 
